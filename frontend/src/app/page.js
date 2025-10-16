7d6ca3a2fd09eaf86b94e35ed696733e304b9cef
djouri <rubyaichan@gmail.com>
2025-10-16 20:23:15 +0100

    create the hero section (needs responsive)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,11 @@
 import Navbar from '@/Components/Navbar';
-import Home from '@/Components/Home'
+import Hero from '@/Components/Hero'
 
 export default function Page() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Hero />
     </div>
   );
 }
